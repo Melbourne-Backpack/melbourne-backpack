eb063efffb0506224d5633906b1c725abf988a9d
Hoang Linh <hlinh.jobs@gmail.com>
2022-03-15 14:08:20 +0700

    testing splash screen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground } from "react-native";
+import { Dimensions } from "react-native";
+
+let width = Dimensions.get("window").width; //full width
+let height = Dimensions.get("window").height; //full height
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Melbourne Backpack 🎉 🎉</Text>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./assets/melbourne-bg.jpg")}
+        style={styles.backgroundImage}
+      >
+        <Text style={styles.text}>Welcome to Melbourne Backpack 🎉 🎉</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -15,7 +23,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  backgroundImage: {
+    width: width,
+    height: height,
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "#ffffff",
   },
 });
