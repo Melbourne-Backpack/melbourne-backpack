4ed45e2820ca7ddf7b0ea579b88ff244a621da6d
doanyennhi <s3880599@rmit.edu.vn>
2022-03-21 13:02:02 +0700

    Add global styling

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,11 @@
-export const PRIMARY = '#1779ba';
-export const SECONDARY = '#767676';
-export const WHITE = '#FFFFFF';
-export const BLACK = '#000000';
+/* PRIMARY COLORS */
+export const BACKGROUND = "#0F0F29";
+export const DARK_BLUE = "#000054";
+export const PURPLE_BLUE = "#4838D1";
+export const LIGHT_BLUE = "#2972FE";
+export const YELLOW = "#FAC800";
+export const RED = "#E61E2A";
+
+/* WHITE AND BLACK */
+export const WHITE = "#FFFFFF";
+export const BLACK = "#000000";
