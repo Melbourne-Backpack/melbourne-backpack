a8d89219dfe1d3941a1a8ffcc42984d3b2bcd262
Hoang Linh <hlinh.jobs@gmail.com>
2022-03-22 14:10:01 +0700

    building Welcome.js front-end

--- src/screens/welcome/styles.js
+++ src/screens/welcome/styles.js
@@ -8,40 +8,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#0f0f29",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  wrapper: {
     alignItems: "flex-start",
     justifyContent: "center",
-    paddingLeft: 60,
+    paddingHorizontal: 40,
   },
   firstText: {
     color: "white",
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "800",
-    opacity: 0.8,
+    opacity: 0.9,
+    paddingBottom: 20,
   },
   secondText: {
     color: "white",
     fontSize: 70,
     fontWeight: "200",
-    opacity: 0.8,
+    opacity: 1.5,
   },
   thirdText: {
     color: "white",
     fontSize: 20,
     fontWeight: "200",
-    opacity: 0.8,
-    paddingRight: 20,
-    paddingTop: 30,
+    opacity: 0.9,
+    paddingTop: 20,
+    paddingBottom: 35,
+  },
+  buttonContainer: {
+    alignSelf: "center",
   },
   viewButton: {
     backgroundColor: "#4838d1",
     borderRadius: 30,
-    paddingLeft: 65,
-    paddingRight: 65,
-    paddingTop: 20,
-    paddingBottom: 20,
-    marginTop: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 60,
   },
-  textButton: { color: "white", fontSize: 15, fontWeight: "600" },
+  textButton: { color: "white", fontSize: 17, fontWeight: "600" },
 });
 
 export default styles;
