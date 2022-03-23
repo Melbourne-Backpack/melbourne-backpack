eab6e43d05913257373b3adbc052896658fa5d11
doanyennhi <s3880599@rmit.edu.vn>
2022-03-24 02:27:25 +0700

    Add icon package, add elements and styling for HousingCard.js

--- src/components/recommendation/HousingCard.js
+++ src/components/recommendation/HousingCard.js
@@ -1,19 +1,31 @@
 import { ImageBackground, Text, View } from "react-native";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+
 import styles from "./styles";
+import { YELLOW } from "../../styles/colors";
 
 const HousingCard = () => {
   return (
     <View style={styles.card}>
-      <View style={styles.container}>
+      <View style={styles.imgContainer}>
         <ImageBackground
           source={require("../../../assets/images/student-housing.jpg")}
           style={styles.backgroundImg}
           resizeMode="cover"
-        />
+        >
+          <View style={styles.overlay}>
+            <Text style={styles.rating}>
+              3.4 <AntDesign name="star" size={20} color={YELLOW} />
+            </Text>
+          </View>
+        </ImageBackground>
       </View>
-      <Text style={styles.text}>Atira</Text>
-      <Text style={styles.text}>$399.00</Text>
-      <Text style={styles.text}>250 Spencer Street, Melbourne, VIC 3000</Text>
+
+      <View style={styles.info}>
+        <Text style={[styles.name, styles.text]}>Atira</Text>
+        <Text style={[styles.price, styles.text]}>$399.00</Text>
+        <Text style={styles.text}>250 Spencer Street, Melbourne, VIC 3000</Text>
+      </View>
     </View>
   );
 };
--- src/components/recommendation/styles.js
+++ src/components/recommendation/styles.js
@@ -1,15 +1,19 @@
 import { StyleSheet } from "react-native";
-import { DARK_BLUE_PURPLE, WHITE } from "../../styles/colors";
-import { scaleSize } from "../../styles/scale";
+import { DARK_BLUE_PURPLE, WHITE, YELLOW } from "../../styles/colors";
+import { scaleFont, scaleSize } from "../../styles/scale";
+import {
+  FONT_WEIGHT_EXTRA_BOLD,
+  FONT_WEIGHT_MEDIUM,
+} from "../../styles/typography";
 
 const styles = StyleSheet.create({
   card: {
-    flex: 0.5,
+    flex: 0.7,
     backgroundColor: DARK_BLUE_PURPLE,
     margin: scaleSize(10),
   },
-  container: {
-    flex: 1,
+  imgContainer: {
+    flex: 0.9,
     position: "relative",
   },
   backgroundImg: {
@@ -19,8 +23,32 @@ const styles = StyleSheet.create({
     bottom: 0,
     right: 0,
   },
+  overlay: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0,0,0, 0.40)",
+  },
+  rating: {
+    color: YELLOW,
+    fontSize: 20,
+    margin: scaleSize(10),
+    fontWeight: FONT_WEIGHT_MEDIUM,
+  },
+  info: {
+    padding: scaleSize(10),
+  },
   text: {
     color: WHITE,
+    marginTop: scaleSize(5),
+  },
+  name: {
+    fontWeight: FONT_WEIGHT_MEDIUM,
+    fontSize: scaleFont(14),
+  },
+  price: {
+    fontWeight: FONT_WEIGHT_EXTRA_BOLD,
+    fontSize: scaleFont(16),
+    marginBottom: scaleSize(2),
   },
 });
 
