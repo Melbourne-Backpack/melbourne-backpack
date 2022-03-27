4061c19061a2add5a1fb618bc1afecff4fbfad23
rider3458 <s3878480@rmit.edu.vn>
2022-03-27 18:25:56 +0700

    push files for testing

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import SplashScreen from "./src/screens/splash/SplashScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/home/HomeScreen";
 import Welcome from "./src/screens/welcome/Welcome";
+import CommunityScreen from "./src/screens/community/CommunityScreen";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Community" component={CommunityScreen} />
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Welcome" component={Welcome} />
