45225f9fe7ea29319b16f38ce161c093f87b2259
Hoang Linh <hlinh.jobs@gmail.com>
2022-03-28 14:56:46 +0700

    resize button Welcome.js

--- src/screens/welcome/styles.js
+++ src/screens/welcome/styles.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#4838d1",
     borderRadius: 25,
     paddingVertical: 20,
-    paddingHorizontal: wp("15%"),
+    paddingHorizontal: wp("14%"),
   },
   textButton: {
     color: WHITE,
