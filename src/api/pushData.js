06c8a566351efa2bc2c8aaa4cb100009dcc67e54
Hoang Linh <hlinh.jobs@gmail.com>
2022-04-02 17:48:24 +0700

    set documents Firestore = user ID

--- src/api/pushData.js
+++ src/api/pushData.js
@@ -1,17 +1,16 @@
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../config/firebase";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore";
+import { db, auth } from "../config/firebase";
 
 const pushData = async (uid, email, fullName, purpose, facebook, bio) => {
   try {
-    const docRef = await addDoc(collection(db, "users"), {
-      uid: uid,
+    await setDoc(doc(db, "users", uid), {
       email: email,
       fullName: fullName,
       purpose: purpose,
       facebook: facebook,
       bio: bio,
     });
-    console.log("Document written with ID: ", docRef.id);
+    console.log("Document written with ID: ", auth.currentUser.uid);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
