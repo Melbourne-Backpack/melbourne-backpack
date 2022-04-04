bfeb326bab33b5a0900c3a7a128df4b3e1c2d2a8
Thu Tran <ngocanhthu20102002@gmail.com>
2022-04-04 13:42:03 +0700

    restyle buttons and bg images

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,36 +1,95 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
 import styles from "./styles";
-import { ImageBackground } from "react-native-web";
-import TouchableOpacity from "react-native-web/dist/exports/TouchableOpacity";
-import Image from "react-native-web/dist/exports/Image";
+import { TouchableOpacity } from "react-native";
+import { useFonts } from "expo-font";
+import { AntDesign } from "@expo/vector-icons";
+import { YELLOW } from "../../styles/colors";
 
 const HomeScreen = ({ navigation }) => {
+  // handle font
+  const [loaded, error] = useFonts({
+    PoppinsSemiBold: require("../../../assets/fonts/Poppins-SemiBold.ttf"),
+    PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsMedium: require("../../../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsExtraBold: require("../../../assets/fonts/Poppins-ExtraBold.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.melbourneBackpackText}>Melbourne Backpack</Text>
-      <View style={styles.container}>
-        <Text style={styles.subtitle}>Community</Text>
-        <ImageBackground
-          resizeMode={"cover"}
-          source={require("../../../assets/images/community.jpg")}
-          style={styles.backgroundImage}
-        />
+      <View style={styles.furtherButton}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/community.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Community</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
       </View>
-      <View style={styles.container}>
-        <Text style={styles.subtitle}>Community</Text>
-        <ImageBackground
-          resizeMode={"cover"}
-          source={require("../../../assets/images/community.jpg")}
-          style={styles.backgroundImage}
-        />
+      <View style={styles.furtherButton}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/housing.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Housing</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
       </View>
-      <View style={styles.container}>
-        <Text style={styles.subtitle}>Community</Text>
-        <ImageBackground
-          resizeMode={"cover"}
-          source={require("../../../assets/images/community.jpg")}
-          style={styles.backgroundImage}
-        />
+      <View style={styles.furtherButton}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/shopping.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Shopping</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.furtherButton}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/transport.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Transport</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.furtherButton}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/profile.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Profile</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
       </View>
     </View>
   );
