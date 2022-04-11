b29c820764e74ae14a0516c1c9a43dc27b1ecb75
rider3458 <s3878480@rmit.edu.vn>
2022-04-11 21:01:26 +0700

    fetch housing data from firestore

--- src/components/recommendation/template/RecommendationTemplate.js
+++ src/components/recommendation/template/RecommendationTemplate.js
@@ -1,197 +1,184 @@
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import {SafeAreaView, ScrollView, Text, View} from "react-native";
 import styles from "./styles";
 import RecommendationCard from "../card/RecommendationCard";
-import { useFonts } from "expo-font";
+import {useFonts} from "expo-font";
 import HousingFilter from "../../housing-filter/HousingFilter";
 
 const type = [
-  {
-    index: 0,
-    name: "apartment",
-  },
-  {
-    index: 1,
-    name: "house",
-  },
-  {
-    index: 2,
-    name: "townhouse",
-  },
+    {
+        index: 0,
+        name: "apartment",
+    },
+    {
+        index: 1,
+        name: "house",
+    },
+    {
+        index: 2,
+        name: "townhouse",
+    },
 ];
 const price = [
-  {
-    index: 0,
-    name: "1-200",
-  },
-  {
-    index: 1,
-    name: "201-400",
-  },
-  {
-    index: 2,
-    name: "401-800",
-  },
-  {
-    index: 3,
-    name: "801-1600",
-  },
-  {
-    index: 4,
-    name: "1601+",
-  },
+    {
+        index: 0,
+        name: "1-200",
+    },
+    {
+        index: 1,
+        name: "201-400",
+    },
+    {
+        index: 2,
+        name: "401-800",
+    },
+    {
+        index: 3,
+        name: "801-1600",
+    },
+    {
+        index: 4,
+        name: "1601+",
+    },
 ];
 const bed = [
-  {
-    index: 0,
-    name: "1",
-  },
-  {
-    index: 1,
-    name: "2",
-  },
-  {
-    index: 2,
-    name: "3",
-  },
-  {
-    index: 3,
-    name: "4",
-  },
-  {
-    index: 4,
-    name: "5+",
-  },
+    {
+        index: 0,
+        name: "1",
+    },
+    {
+        index: 1,
+        name: "2",
+    },
+    {
+        index: 2,
+        name: "3",
+    },
+    {
+        index: 3,
+        name: "4",
+    },
+    {
+        index: 4,
+        name: "5+",
+    },
 ];
 const bath = [
-  {
-    index: 0,
-    name: "1",
-  },
-  {
-    index: 1,
-    name: "2",
-  },
-  {
-    index: 2,
-    name: "3",
-  },
-  {
-    index: 3,
-    name: "4",
-  },
-  {
-    index: 4,
-    name: "5+",
-  },
+    {
+        index: 0,
+        name: "1",
+    },
+    {
+        index: 1,
+        name: "2",
+    },
+    {
+        index: 2,
+        name: "3",
+    },
+    {
+        index: 3,
+        name: "4",
+    },
+    {
+        index: 4,
+        name: "5+",
+    },
 ];
 const area = [
-  {
-    index: 0,
-    name: "1-50",
-  },
-  {
-    index: 1,
-    name: "51-100",
-  },
-  {
-    index: 2,
-    name: "101-200",
-  },
-  {
-    index: 3,
-    name: "201-400",
-  },
-  {
-    index: 4,
-    name: "401+",
-  },
+    {
+        index: 0,
+        name: "1-50",
+    },
+    {
+        index: 1,
+        name: "51-100",
+    },
+    {
+        index: 2,
+        name: "101-200",
+    },
+    {
+        index: 3,
+        name: "201-400",
+    },
+    {
+        index: 4,
+        name: "401+",
+    },
 ];
 
 const distanceFromRMIT = [
-  {
-    index: 0,
-    name: "0-5",
-  },
-  {
-    index: 1,
-    name: "5-10",
-  },
-  {
-    index: 2,
-    name: "10-15",
-  },
-  {
-    index: 3,
-    name: "15+",
-  },
+    {
+        index: 0,
+        name: "0-1",
+    },
+    {
+        index: 1,
+        name: "2-5",
+    },
+    {
+        index: 2,
+        name: "5-10",
+    },
+    {
+        index: 3,
+        name: "10+",
+    },
 ];
 
-const RecommendationTemplate = ({ topic, data, housing, navigation }) => {
-  let highestRating = 0;
-  let trendingCard = {};
+const RecommendationTemplate = ({topic, data, housing, navigation}) => {
+    let highestRating = 0;
+    let trendingCard = {};
 
-  if (housing) {
-    data.map((item) => {
-      if (parseFloat(item.rating) > highestRating) {
-        highestRating = parseFloat(item.rating);
-        trendingCard = item;
-      }
+    if (data.length > 0) {
+        trendingCard = data[0]
+    }
+
+    const [loaded, error] = useFonts({
+        PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
+        PoppinsSemiBold: require("../../../../assets/fonts/Poppins-SemiBold.ttf"),
+        PoppinsMedium: require("../../../../assets/fonts/Poppins-Medium.ttf"),
     });
-  } else {
-    trendingCard = data[0];
-  }
 
-  const [loaded, error] = useFonts({
-    PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
-    PoppinsSemiBold: require("../../../../assets/fonts/Poppins-SemiBold.ttf"),
-    PoppinsMedium: require("../../../../assets/fonts/Poppins-Medium.ttf"),
-  });
+    if (!loaded) {
+        return null;
+    }
 
-  if (!loaded) {
-    return null;
-  }
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View style={styles.wrapper}>
+                    <Text style={styles.heading1}>Trending {topic}</Text>
+                    {
+                        data !== [] ? <RecommendationCard data={trendingCard} housing={housing}/> : null
+                    }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.wrapper}>
-          <Text style={styles.heading1}>Trending {topic}</Text>
-          <RecommendationCard data={trendingCard} housing={housing} />
+                    <View style={styles.secondHeader}>
+                        <Text style={styles.heading2}>All</Text>
+                    </View>
 
-          <View style={styles.secondHeader}>
-            <Text style={styles.heading2}>All</Text>
-          </View>
-
-          <View style={styles.filterOptions}>
-            {housing ? (
-              <HousingFilter
-                headingList={[
-                  "type",
-                  "price",
-                  "bedroom",
-                  "bathroom",
-                  "area",
-                  "distance from RMIT",
-                ]}
-                optionList={[type, price, bed, bath, area, distanceFromRMIT]}
-                navigation={navigation}
-                housingList={data}
-                isHousing={true}
-              />
-            ) : (
-              <HousingFilter
-                headingList={[]}
-                optionList={[]}
-                navigation={navigation}
-                housingList={data}
-                isHousing={false}
-              />
-            )}
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+                    <View style={styles.filterOptions}>
+                        {data.length > 0 ? (
+                            <HousingFilter
+                                headingList={[
+                                    "type",
+                                    "price",
+                                    "bed",
+                                    "bath",
+                                    "distance from RMIT",
+                                ]}
+                                optionList={[type, price, bed, bath, distanceFromRMIT]}
+                                navigation={navigation}
+                                housingList={data}
+                                isHousing={true}
+                            />
+                        ) : null
+                        }
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default RecommendationTemplate;
--- src/screens/community/CommunityScreen.js
+++ src/screens/community/CommunityScreen.js
@@ -1,168 +1,168 @@
-import { Text, View, ScrollView, FlatList, SafeAreaView } from "react-native";
+import {Text, View, ScrollView, FlatList, SafeAreaView} from "react-native";
 import CommunityCardLarge from "../../components/community-card/community-card-large/CommunityCardLarge";
 import CommunityFilter from "../../components/community-filter/CommunityFilter";
 import styles from "./styles";
 import data from "../../../assets/mockJSON/MOCK_DATA.json";
-import { useFonts } from "expo-font";
+import {useFonts} from "expo-font";
 
-const CommunityScreen = ({ navigation }) => {
-  const mostLikeYouMaxCards = 4;
-  const self = "1";
+const CommunityScreen = ({navigation}) => {
+    const mostLikeYouMaxCards = 4;
+    const self = "1";
 
-  const [loaded, error] = useFonts({
-    PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
-    PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
-  const mostLikeYouData = () => {
-    let temp = [];
-    let myCampus = "";
-    let myTopic = [];
-    data.map((user) => {
-      let id = user.index;
-      if (id.toString() === self.toString()) {
-        myCampus = user.campus;
-        myTopic = user.topic;
-      }
+    const [loaded, error] = useFonts({
+        PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
+        PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
     });
 
-    for (let i = 0; i < data.length; i++) {
-      let user = data[i];
-      let id = user.index;
-
-      if (
-        id.toString() !== self.toString() &&
-        user.campus === myCampus &&
-        !temp.includes(id.toString())
-      ) {
-        temp.push(user);
-      }
-
-      if (
-        data.indexOf(user) === data.length - 1 &&
-        temp.length !== mostLikeYouMaxCards
-      ) {
-        for (let i = 0; i < data.length; i++) {
-          if (
-            id.toString() !== self.toString() &&
-            !temp.includes(id.toString())
-          ) {
-            myTopic.map((topic) => {
-              if (user.topic.includes(topic)) {
-                temp.push(user);
-              }
-            });
-          }
-        }
-      }
+    if (!loaded) {
+        return null;
     }
-    return temp;
-  };
 
-  const mostLikeYouDataForDisplay = mostLikeYouData().splice(
-    0,
-    mostLikeYouMaxCards
-  );
-  const campus = [
-    {
-      id: 1,
-      name: "SGS Campus",
-    },
-    {
-      id: 2,
-      name: "Hanoi Campus",
-    },
-    {
-      id: 3,
-      name: "Danang Campus",
-    },
-  ];
+    const mostLikeYouData = () => {
+        let temp = [];
+        let myCampus = "";
+        let myTopic = [];
+        data.map((user) => {
+            let id = user.index;
+            if (id.toString() === self.toString()) {
+                myCampus = user.campus;
+                myTopic = user.topic;
+            }
+        });
 
-  const topic = [
-    {
-      id: 1,
-      name: "Economics",
-    },
-    {
-      id: 2,
-      name: "Business",
-    },
-    {
-      id: 3,
-      name: "Robotics",
-    },
-    {
-      id: 4,
-      name: "Management",
-    },
-    {
-      id: 5,
-      name: "Aviation",
-    },
-    {
-      id: 6,
-      name: "Education",
-    },
-    {
-      id: 7,
-      name: "Design",
-    },
-    {
-      id: 8,
-      name: "Prof Com",
-    },
-    {
-      id: 9,
-      name: "Technology",
-    },
-    {
-      id: 10,
-      name: "Digital Marketing",
-    },
-  ];
+        for (let i = 0; i < data.length; i++) {
+            let user = data[i];
+            let id = user.index;
 
-  return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      <View style={styles.mostLikeYouContainer}>
-        <Text style={styles.mostLikeYouText}>Most like you</Text>
-        <SafeAreaView>
-          <FlatList
-            style={styles.mostLikeYou}
-            data={mostLikeYouDataForDisplay}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            renderItem={(user) => {
-              return (
-                <CommunityCardLarge
-                  userID={user.item.index}
-                  name={user.item.name}
-                  campus={user.item.campus}
-                  picture={user.item.picture}
-                  topic={user.item.topic[Math.floor(Math.random() * 2)]}
-                  navigation={navigation}
-                />
-              );
-            }}
-          />
-        </SafeAreaView>
-      </View>
-      <View style={styles.communityListContainer}>
-        <Text style={styles.communityListText}>Community</Text>
-        <View>
-          <CommunityFilter
-            headingList={["campus", "topic"]}
-            optionList={[campus, topic]}
-            navigation={navigation}
-            userList={data}
-          />
-        </View>
-      </View>
-    </ScrollView>
-  );
+            if (
+                id.toString() !== self.toString() &&
+                user.campus === myCampus &&
+                !temp.includes(id.toString())
+            ) {
+                temp.push(user);
+            }
+
+            if (
+                data.indexOf(user) === data.length - 1 &&
+                temp.length !== mostLikeYouMaxCards
+            ) {
+                for (let i = 0; i < data.length; i++) {
+                    if (
+                        id.toString() !== self.toString() &&
+                        !temp.includes(id.toString())
+                    ) {
+                        myTopic.map((topic) => {
+                            if (user.topic.includes(topic)) {
+                                temp.push(user);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+        return temp;
+    };
+
+    const mostLikeYouDataForDisplay = mostLikeYouData().splice(
+        0,
+        mostLikeYouMaxCards
+    );
+    const campus = [
+        {
+            id: 1,
+            name: "SGS",
+        },
+        {
+            id: 2,
+            name: "Hanoi",
+        },
+        {
+            id: 3,
+            name: "Danang",
+        },
+    ];
+
+    const topic = [
+        {
+            id: 1,
+            name: "Economics",
+        },
+        {
+            id: 2,
+            name: "Business",
+        },
+        {
+            id: 3,
+            name: "Robotics",
+        },
+        {
+            id: 4,
+            name: "Management",
+        },
+        {
+            id: 5,
+            name: "Aviation",
+        },
+        {
+            id: 6,
+            name: "Education",
+        },
+        {
+            id: 7,
+            name: "Design",
+        },
+        {
+            id: 8,
+            name: "Prof Com",
+        },
+        {
+            id: 9,
+            name: "Technology",
+        },
+        {
+            id: 10,
+            name: "Digital Marketing",
+        },
+    ];
+
+    return (
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+            <View style={styles.mostLikeYouContainer}>
+                <Text style={styles.mostLikeYouText}>Most like you</Text>
+                <SafeAreaView>
+                    <FlatList
+                        style={styles.mostLikeYou}
+                        data={mostLikeYouDataForDisplay}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={(user) => {
+                            return (
+                                <CommunityCardLarge
+                                    userID={user.item.index}
+                                    name={user.item.name}
+                                    campus={user.item.campus}
+                                    picture={user.item.picture}
+                                    topic={user.item.topic[Math.floor(Math.random() * 2)]}
+                                    navigation={navigation}
+                                />
+                            );
+                        }}
+                    />
+                </SafeAreaView>
+            </View>
+            <View style={styles.communityListContainer}>
+                <Text style={styles.communityListText}>Community</Text>
+                <View>
+                    <CommunityFilter
+                        headingList={["campus", "topic"]}
+                        optionList={[campus, topic]}
+                        navigation={navigation}
+                        userList={data}
+                    />
+                </View>
+            </View>
+        </ScrollView>
+    );
 };
 
 export default CommunityScreen;
