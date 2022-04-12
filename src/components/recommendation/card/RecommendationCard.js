cf07ace629311fdf1b7157cdc20893154f121cd7
doanyennhi <s3880599@rmit.edu.vn>
2022-04-12 22:11:28 +0700

    Change navigation

--- src/components/recommendation/card/RecommendationCard.js
+++ src/components/recommendation/card/RecommendationCard.js
@@ -1,82 +1,83 @@
-import {ImageBackground, Text, TouchableOpacity, View} from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import {
-    AntDesign,
-    MaterialCommunityIcons,
-    Ionicons,
+  AntDesign,
+  MaterialCommunityIcons,
+  Ionicons,
 } from "@expo/vector-icons";
 
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
-import {GREY, YELLOW} from "../../../styles/colors";
-import {useFonts} from "expo-font";
+import { GREY, YELLOW } from "../../../styles/colors";
+import { useFonts } from "expo-font";
 
-const RecommendationCard = ({data, housing}) => {
-    const [loaded, error] = useFonts({
-        PoppinsExtraBold: require("../../../../assets/fonts/Poppins-ExtraBold.ttf"),
-        PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
-        PoppinsSemiBold: require("../../../../assets/fonts/Poppins-SemiBold.ttf"),
-    });
+const RecommendationCard = ({ data, housing }) => {
+  const navigation = useNavigation();
+  const [loaded, error] = useFonts({
+    PoppinsExtraBold: require("../../../../assets/fonts/Poppins-ExtraBold.ttf"),
+    PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsSemiBold: require("../../../../assets/fonts/Poppins-SemiBold.ttf"),
+  });
 
-    if (!loaded) {
-        return null;
-    }
-  
-    return (
-        <View style={styles.card}>
-            <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => navigation.navigate("Details")}
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.card}>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() => navigation.navigate("Details")}
+      >
+        <ImageBackground
+          source={{ uri: data.image }}
+          style={styles.backgroundImg}
+          imageStyle={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
+          resizeMode="cover"
         >
-                <ImageBackground
-                    source={{uri: data.image}}
-                    style={styles.backgroundImg}
-                    imageStyle={{borderTopLeftRadius: 20, borderTopRightRadius: 20}}
-                    resizeMode="cover"
-                >
-                    <View style={styles.overlay}>
-                        {housing !== true ? (
-                            <></>
-                        ) : (
-                            <Text style={styles.rating}>
-                                {data.rating} <AntDesign name="star" size={30} color={YELLOW}/>
-                            </Text>
-                        )}
-                    </View>
-                </ImageBackground>
-            </TouchableOpacity>
+          <View style={styles.overlay}>
+            {housing !== true ? (
+              <></>
+            ) : (
+              <Text style={styles.rating}>
+                {data.rating} <AntDesign name="star" size={30} color={YELLOW} />
+              </Text>
+            )}
+          </View>
+        </ImageBackground>
+      </TouchableOpacity>
 
-            <View style={styles.info}>
-                <TouchableOpacity onPress={() => navigation.navigate("Details")}>
-                    <Text style={[styles.name, styles.text]}>{data.title}</Text>
-                </TouchableOpacity>
-                {housing !== true ? (
-                    <></>
-                ) : (
-                    <Text style={[styles.price, styles.text]}>${data.price}</Text>
-                )}
-                <Text style={[styles.text, styles.location]}>
-                    <Ionicons name="location-sharp" size={16} color="white"/>{" "}
-                    {data.address}
-                </Text>
+      <View style={styles.info}>
+        <TouchableOpacity onPress={() => navigation.navigate("Details")}>
+          <Text style={[styles.name, styles.text]}>{data.title}</Text>
+        </TouchableOpacity>
+        {housing !== true ? (
+          <></>
+        ) : (
+          <Text style={[styles.price, styles.text]}>${data.price}</Text>
+        )}
+        <Text style={[styles.text, styles.location]}>
+          <Ionicons name="location-sharp" size={16} color="white" />{" "}
+          {data.address}
+        </Text>
 
-                {housing !== true ? (
-                    <></>
-                ) : (
-                    <View style={styles.extraOuterContainer}>
-                        <View style={styles.extraContainer}>
-                            <Text style={styles.extra}>{data.bed}</Text>
-                            <Ionicons name="bed" size={35} color={GREY}/>
-                        </View>
-
-                        <View style={styles.extraContainer}>
-                            <Text style={styles.extra}>{data.bath}</Text>
-                            <MaterialCommunityIcons name="shower" size={35} color={GREY}/>
-                        </View>
-                    </View>
-                )}
+        {housing !== true ? (
+          <></>
+        ) : (
+          <View style={styles.extraOuterContainer}>
+            <View style={styles.extraContainer}>
+              <Text style={styles.extra}>{data.bed}</Text>
+              <Ionicons name="bed" size={35} color={GREY} />
             </View>
-        </View>
-    );
+
+            <View style={styles.extraContainer}>
+              <Text style={styles.extra}>{data.bath}</Text>
+              <MaterialCommunityIcons name="shower" size={35} color={GREY} />
+            </View>
+          </View>
+        )}
+      </View>
+    </View>
+  );
 };
 
 export default RecommendationCard;
