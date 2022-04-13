7ca8a97d0cfa24b6a8562b04e596e138c3f32ca9
rider3458 <s3878480@rmit.edu.vn>
2022-04-13 10:12:02 +0700

    Add Profile screen navigation to the main stack navigator The profile btn on the bottom nav will not be focused on press, open a new screen without nav for the current user instead The profile page still works when a user card is pressed

--- src/navigation/content-navigator/ContentNavigator.js
+++ src/navigation/content-navigator/ContentNavigator.js
@@ -2,98 +2,111 @@ import HousingScreen from "../../screens/recommendation/HousingScreen";
 import TransportScreen from "../../screens/recommendation/TransportScreen";
 import ShoppingScreen from "../../screens/recommendation/ShoppingScreen";
 import CommunityScreen from "../../screens/community/CommunityScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import styles from "./styles";
-import { LIGHT_PURPLE, LIGHTER_GREY } from "../../styles/colors";
+import {LIGHT_PURPLE, LIGHTER_GREY} from "../../styles/colors";
 import Profile from "../../screens/profile/Profile";
-import { Image } from "react-native";
+import {Image} from "react-native";
+import {auth} from "../../config/firebase";
+import {TouchableHighlight} from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const tabIcons = [
-  {
-    route: "Housing",
-    iconName: "home-outline",
-    iconFocus: "home",
-  },
-  {
-    route: "Transport",
-    iconName: "train-outline",
-    iconFocus: "train",
-  },
-  {
-    route: "Shopping",
-    iconName: "md-cart-outline",
-    iconFocus: "md-cart",
-  },
-  {
-    route: "Community",
-    iconName: "md-people-outline",
-    iconFocus: "md-people",
-  },
-  {
-    route: "Profile",
-  },
+    {
+        route: "Housing",
+        iconName: "home-outline",
+        iconFocus: "home",
+    },
+    {
+        route: "Transport",
+        iconName: "train-outline",
+        iconFocus: "train",
+    },
+    {
+        route: "Shopping",
+        iconName: "md-cart-outline",
+        iconFocus: "md-cart",
+    },
+    {
+        route: "Community",
+        iconName: "md-people-outline",
+        iconFocus: "md-people",
+    },
+    {
+        route: "Profile",
+    },
 ];
 
-const ContentNavigator = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused }) => {
-          let iconName, iconColor, icon;
-          let iconSize = focused ? 38 : 28;
+const ContentNavigator = ({navigation}) => {
+    return (
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused}) => {
+                    let iconName, iconColor, icon;
+                    let iconSize = focused ? 38 : 28;
 
-          tabIcons.map((tabIcon) => {
-            if (route.name === tabIcon.route) {
-              if (route.name === "Profile") {
-                icon = (
-                  <Image
-                    source={require("../../../assets/images/avatar-placeholder.jpg")}
-                    style={
-                      focused
-                        ? {
-                            width: iconSize,
-                            height: iconSize,
-                            borderRadius: 50,
-                            borderColor: LIGHT_PURPLE,
-                            borderWidth: 3,
-                          }
-                        : {
-                            width: iconSize,
-                            height: iconSize,
-                            borderRadius: 50,
-                          }
-                    }
-                  />
-                );
-              } else {
-                iconName = focused ? tabIcon.iconFocus : tabIcon.iconName;
-                iconColor = focused ? LIGHT_PURPLE : LIGHTER_GREY;
-                icon = (
-                  <Ionicons name={iconName} size={iconSize} color={iconColor} />
-                );
-              }
-              // }
-            }
-          });
-          return icon;
-        },
-        headerShown: false,
-        tabBarStyle: styles.tabBar,
-        tabBarShowLabel: false,
-      })}
-    >
-      <Tab.Screen name="Housing" component={HousingScreen} />
-      <Tab.Screen name="Transport" component={TransportScreen} />
-      <Tab.Screen name="Shopping" component={ShoppingScreen} />
-      <Tab.Screen name="Community" component={CommunityScreen} />
-      <Tab.Screen name="Profile" component={Profile} />
-    </Tab.Navigator>
-  );
+                    tabIcons.map((tabIcon) => {
+                        if (route.name === tabIcon.route) {
+                            if (route.name === "Profile") {
+                                icon = (
+                                    <TouchableHighlight onPress={() => {
+                                        navigation.navigate('Profile', {
+                                            params: {user: auth.currentUser.uid},
+                                        })
+                                    }}>
+                                        <Image
+                                            source={require("../../../assets/images/avatar-placeholder.jpg")}
+                                            style={
+                                                // focused
+                                                //     ? {
+                                                //         width: iconSize,
+                                                //         height: iconSize,
+                                                //         borderRadius: 50,
+                                                //         borderColor: LIGHT_PURPLE,
+                                                //         borderWidth: 3,
+                                                //     }
+                                                //     : {
+                                                //         width: iconSize,
+                                                //         height: iconSize,
+                                                //         borderRadius: 50,
+                                                //     }
+                                                {
+                                                    width: 28,
+                                                    height: 28,
+                                                    borderRadius: 50,
+                                                }
+                                            }
+                                        />
+                                    </TouchableHighlight>
+                                );
+                            } else {
+                                iconName = focused ? tabIcon.iconFocus : tabIcon.iconName;
+                                iconColor = focused ? LIGHT_PURPLE : LIGHTER_GREY;
+                                icon = (
+                                    <Ionicons name={iconName} size={iconSize} color={iconColor}/>
+                                );
+                            }
+                            // }
+                        }
+                    });
+                    return icon;
+                },
+                headerShown: false,
+                tabBarStyle: styles.tabBar,
+                tabBarShowLabel: false,
+            })}
+        >
+            <Tab.Screen name="Housing" component={HousingScreen}/>
+            <Tab.Screen name="Transport" component={TransportScreen}/>
+            <Tab.Screen name="Shopping" component={ShoppingScreen}/>
+            <Tab.Screen name="Community" component={CommunityScreen}/>
+            <Tab.Screen name="Profile" component={Profile}/>
+        </Tab.Navigator>
+    );
 };
 
 export default ContentNavigator;
