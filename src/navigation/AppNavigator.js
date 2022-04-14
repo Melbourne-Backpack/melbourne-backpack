34599848e45280eb81867c30e9598cda196ab557
Hoang Linh <hlinh.jobs@gmail.com>
2022-04-14 21:10:27 +0700

    - add fade effect transition

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -16,11 +16,18 @@ import HousingDetailScreen from "../screens/housing-details/HousingDetailScreen"
 const AppNavigator = () => {
   const Stack = createStackNavigator();
 
+  const forFade = ({ current }) => ({
+    cardStyle: {
+      opacity: current.progress,
+    },
+  });
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
         gestureEnabled: false,
+        cardStyleInterpolator: forFade,
       }}
     >
       <Stack.Screen name="Splash" component={SplashScreen} />
