8e121cedefc01581cc8a0422ee67900a764192a5
rider3458 <s3878480@rmit.edu.vn>
2022-04-15 19:45:06 +0700

    Move capitalization function to another file

--- src/utils/Capitalize.js
+++ src/utils/Capitalize.js
@@ -1,9 +1,14 @@
 const Capitalize = (input) => {
     // To use with fetched data, use a condition to make sure that the input string is not undefined.
-    let temp = input
-    for (let i = 1; i < temp.length; i++) {
-        if (temp[i - 1] === " " || temp > 1 && temp.slice(i - 2, i) === ", ") {
-            temp = temp.slice(0, i) + temp[i].toUpperCase() + temp.slice(i + 1, temp.length)
+    let temp = input[0].toUpperCase()
+    if (input.length > 1) {
+        temp += input.slice(1, input.length)
+    }
+    if (temp.length > 1) {
+        for (let i = 1; i < temp.length; i++) {
+            if (temp[i - 1] === " " || temp > 1 && temp.slice(i - 2, i) === ", ") {
+                temp = temp.slice(0, i) + temp[i].toUpperCase() + temp.slice(i + 1, temp.length)
+            }
         }
     }
 
