cc363157e21dfa1db4c59249f02a4e5d1ca04a49
Thu Tran <ngocanhthu20102002@gmail.com>
2022-04-18 12:38:58 +0700

    validate sign up

--- src/screens/login/SignUp.js
+++ src/screens/login/SignUp.js
@@ -15,11 +15,81 @@ import { useFonts } from "expo-font";
 import React, { useEffect, useState } from "react";
 import { WHITE } from "../../styles/colors";
 import { signUp } from "../../api/loginApi";
+import * as Animatable from "react-native-animatable";
+import Feather from "react-native-vector-icons/Feather";
 
 const SignUp = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [cfPassword, setCfPassword] = useState("");
+
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    cfPassword: "",
+    check_textInputChange: false,
+    secureTextEntry: true,
+    confirm_secureTextEntry: true,
+  });
+
+  const textInputChange = (val) => {
+    if (val.trim().regexp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+      setData({
+        ...data,
+        email: val,
+        check_textInputChange: true,
+      });
+    } else {
+      setData({
+        ...data,
+        email: val,
+        check_textInputChange: false,
+      });
+    }
+  };
+
+  const handlePasswordChange = (val) => {
+    if (val.trim().length >= 8) {
+      setData({
+        ...data,
+        password: val,
+      });
+    } else {
+      setData({
+        ...data,
+        password: val,
+      });
+    }
+  };
+
+  const handleCfPasswordChange = (val) => {
+    if (val.trim() === password) {
+      setData({
+        ...data,
+        cfPassword: val,
+        isValidPassword: true,
+      });
+    } else {
+      setData({
+        ...data,
+        cfPassword: val,
+      });
+    }
+  };
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
+  const updateConfirmSecureTextEntry = () => {
+    setData({
+      ...data,
+      confirm_secureTextEntry: !data.confirm_secureTextEntry,
+    });
+  };
 
   const [loaded, error] = useFonts({
     PoppinsSemiBold: require("../../../assets/fonts/Poppins-SemiBold.ttf"),
@@ -50,41 +120,65 @@ const SignUp = ({ navigation }) => {
                 placeholder={"Email"}
                 keyboardType={"email-address"}
                 placeholderTextColor={WHITE}
-                onChangeText={(text) => setEmail(text)}
                 defaultValue={email}
+                autoCapitalize="none"
+                onChangeText={(val) => textInputChange(val)}
               />
-              <TextInput
-                style={styles.textInput}
-                placeholder={"Password"}
-                placeholderTextColor={WHITE}
-                onChangeText={(text) => setPassword(text)}
-                defaultValue={password}
-                secureTextEntry={true}
-              />
-              <TextInput
-                style={styles.textInput}
-                placeholder={"Confirm Password"}
-                placeholderTextColor={WHITE}
-                onChangeText={(text) => setCfPassword(text)}
-                defaultValue={cfPassword}
-                secureTextEntry={true}
-              />
-              <TouchableOpacity
-                onPress={() => {
-                  signUp({ navigation }, email, password, cfPassword);
-                }}
-              >
-                <View style={styles.loginButtonView}>
-                  <Text style={styles.loginButtonText}>Sign Up</Text>
-                </View>
-              </TouchableOpacity>
+              {data.check_textInputChange ? (
+                <Animatable.View animation="bounceIn">
+                  <Feather name="check-circle" color="green" size={20} />
+                </Animatable.View>
+              ) : null}
             </View>
-            <View style={styles.field}>
-              <Text style={styles.label}>Already got an account?</Text>
-              <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-                <Text style={styles.registerButtonText}>Sign In</Text>
-              </TouchableOpacity>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder={"Password"}
+              placeholderTextColor={WHITE}
+              onChangeText={(text) => setPassword(text)}
+              defaultValue={password}
+              secureTextEntry={true}
+            />
+            <TouchableOpacity onPress={updateSecureTextEntry}>
+              {data.secureTextEntry ? (
+                <Feather name="eye-off" color="grey" size={20} />
+              ) : (
+                <Feather name="eye" color="grey" size={20} />
+              )}
+            </TouchableOpacity>
+          </View>
+
+          <TextInput
+            style={styles.textInput}
+            placeholder={"Confirm Password"}
+            placeholderTextColor={WHITE}
+            onChangeText={(text) => setCfPassword(text)}
+            defaultValue={cfPassword}
+            secureTextEntry={true}
+          />
+          <TouchableOpacity onPress={updateConfirmSecureTextEntry}>
+            {data.secureTextEntry ? (
+              <Feather name="eye-off" color="grey" size={20} />
+            ) : (
+              <Feather name="eye" color="grey" size={20} />
+            )}
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              signUp({ navigation }, email, password, cfPassword);
+            }}
+          >
+            <View style={styles.loginButtonView}>
+              <Text style={styles.loginButtonText}>Sign Up</Text>
             </View>
+          </TouchableOpacity>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Already got an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+              <Text style={styles.registerButtonText}>Sign In</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
@@ -93,5 +187,3 @@ const SignUp = ({ navigation }) => {
 };
 
 export default SignUp;
-
-//🇦🇺 🇦🇺
