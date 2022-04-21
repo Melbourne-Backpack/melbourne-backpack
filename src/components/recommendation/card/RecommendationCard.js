d362802618145320021ad53992f94c0ae0435570
doanyennhi <s3880599@rmit.edu.vn>
2022-04-22 00:52:40 +0700

    Add images, process data fetched from public API

--- src/components/recommendation/card/RecommendationCard.js
+++ src/components/recommendation/card/RecommendationCard.js
@@ -6,26 +6,28 @@ import {
 } from "@expo/vector-icons";
 
 import styles from "./styles";
-import {useNavigation} from "@react-navigation/native";
-import {GREY, YELLOW} from "../../../styles/colors";
-import {useFonts} from "expo-font";
-import {useEffect, useState} from "react";
+import { useNavigation } from "@react-navigation/native";
+import { GREY, YELLOW } from "../../../styles/colors";
+import { useFonts } from "expo-font";
+import { useEffect, useState } from "react";
 import Capitalize from "../../../utils/Capitalize";
 
-const RecommendationCard = ({ data, housing }) => {
+const RecommendationCard = ({ data, housing, routeType }) => {
   const navigation = useNavigation();
   const [address, setAddress] = useState("");
+  console.log(routeType);
+
   const [loaded, error] = useFonts({
     PoppinsExtraBold: require("../../../../assets/fonts/Poppins-ExtraBold.ttf"),
     PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
     PoppinsSemiBold: require("../../../../assets/fonts/Poppins-SemiBold.ttf"),
   });
-  
+
   useEffect(() => {
-        if (data.address !== undefined) {
-            setAddress(Capitalize(data.address))
-        }
-    }, [data])
+    if (data.address !== undefined) {
+      setAddress(Capitalize(data.address));
+    }
+  }, [data]);
 
   if (!loaded) {
     return null;
@@ -62,11 +64,11 @@ const RecommendationCard = ({ data, housing }) => {
               imageStyle={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
               resizeMode="cover"
             >
-              <View style={styles.overlay}>          
-                  <Text style={styles.rating}>
-                    {data.rating}{" "}
-                    <AntDesign name="star" size={30} color={YELLOW} />
-                  </Text>
+              <View style={styles.overlay}>
+                <Text style={styles.rating}>
+                  {data.rating}{" "}
+                  <AntDesign name="star" size={30} color={YELLOW} />
+                </Text>
               </View>
             </ImageBackground>
           </TouchableOpacity>
@@ -75,18 +77,24 @@ const RecommendationCard = ({ data, housing }) => {
             <TouchableOpacity
               onPress={() => navigation.navigate("Details", { id: data.id })}
             >
-              <Text
-                        style={[styles.name, styles.text]}>{data.title ? data.title[0].toUpperCase() + data.title.slice(1, data.title.length) : data.title}</Text>
+              <Text style={[styles.name, styles.text]}>
+                {data.title
+                  ? data.title[0].toUpperCase() +
+                    data.title.slice(1, data.title.length)
+                  : data.title}
+              </Text>
             </TouchableOpacity>
-             <Text
-                        style={[styles.price, styles.text]}>
-                        {typeof data.price !== "undefined" ? (data.price.length > 4 && data
-                                .price.substring(0, 4) === "from" ? "From $" + data.price.split(' ')[1] : "$" + data.price
-                        ) : null}</Text>
+            <Text style={[styles.price, styles.text]}>
+              {typeof data.price !== "undefined"
+                ? data.price.length > 4 && data.price.substring(0, 4) === "from"
+                  ? "From $" + data.price.split(" ")[1]
+                  : "$" + data.price
+                : null}
+            </Text>
             <Text style={[styles.text, styles.location]}>
-                    <Ionicons name="location-sharp" size={16} color="white"/>{" "}
-                    {address}
-                </Text>
+              <Ionicons name="location-sharp" size={16} color="white" />{" "}
+              {address}
+            </Text>
 
             <View style={styles.extraOuterContainer}>
               <View style={styles.extraContainer}>
--- src/screens/recommendation/TransportScreen.js
+++ src/screens/recommendation/TransportScreen.js
@@ -1,17 +1,71 @@
 import RecommendationTemplate from "../../components/recommendation/template/RecommendationTemplate";
 import { useEffect, useState } from "react";
 
-import { DEVID, SIGNATURE } from "@env";
+import { DEVID, ROUTE_TYPE_SIGNATURE, TRANSPORT_SIGNATURE } from "@env";
 
 const TransportScreen = () => {
+  const [rawData, setRawData] = useState([]);
+  const [routeType, setRouteType] = useState([]);
   const [transportData, setTransportData] = useState([]);
-  const url = `http://timetableapi.ptv.vic.gov.au/v3/stops/location/-37.808974,144.965272?devid=${DEVID}&signature=${SIGNATURE}`;
+  const typeUrl = `http://timetableapi.ptv.vic.gov.au/v3/route_types?devid=${DEVID}&signature=${ROUTE_TYPE_SIGNATURE}`;
+
+  const lat = "-37.8080770201347";
+  const long = "144.96268921184907";
+  const dataUrl = `http://timetableapi.ptv.vic.gov.au/v3/stops/location/${lat},${long}?devid=${DEVID}&signature=${TRANSPORT_SIGNATURE}`;
+
+  const getRouteType = () => {
+    fetch(typeUrl)
+      .then((res) => res.json())
+      .then((data) => setRouteType(data["route_types"]));
+  };
+
+  const getRawData = () => {
+    fetch(dataUrl)
+      .then((res) => res.json())
+      .then((data) => setRawData(data["stops"]));
+  };
+
+  const defineRouteType = (type) => {
+    let routeName = "";
+    routeType.map((route) => {
+      if (route["route_type"] === type) {
+        routeName = route["route_type_name"];
+      }
+    });
+    return routeName;
+  };
+
+  const getRouteNumAndName = (routes) => {
+    const routeInfo = [];
+    routes.map((route) => {
+      routeInfo.push({
+        routeName: route["route_name"],
+        routNum: route["route_number"],
+      });
+    });
+    return routeInfo;
+  };
+
+  const processRawData = () => {
+    const processedData = [];
+    rawData.map((data) => {
+      processedData.push({
+        stopName: data["stop_name"],
+        stopLat: data["stop_latitude"],
+        stopLong: data["stop_longitude"],
+        transportationType: defineRouteType(data["route_type"]),
+        route: getRouteNumAndName(data["routes"]),
+      });
+    });
+    return processedData;
+  };
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => console.log(data.stops));
+    getRouteType();
+    getRawData();
   }, []);
+
+  console.log(processRawData());
 
   const data = [
     {
