43d6e3450516d5030c8ddd2b7d81005adcf7faee
rider3458 <s3878480@rmit.edu.vn>
2022-04-25 12:00:03 +0700

    remove placeholder images

--- src/components/community-card/community-card-small/CommunityCardSmall.js
+++ src/components/community-card/community-card-small/CommunityCardSmall.js
@@ -1,49 +1,49 @@
 import {
-    Image,
-    ImageBackground,
-    Text,
-    TouchableOpacity,
-    View,
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import React from "react";
 import styles from "./styles";
-import {useFonts} from "expo-font";
+import { useFonts } from "expo-font";
 
-const CommunityCardSmall = ({userID, name, navigation, picture}) => {
-    const [loaded, error] = useFonts({
-        PoppinsExtraBold: require("../../../../assets/fonts/Poppins-ExtraBold.ttf"),
-        PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
-    });
+const CommunityCardSmall = ({ userID, name, navigation, picture }) => {
+  const [loaded, error] = useFonts({
+    PoppinsExtraBold: require("../../../../assets/fonts/Poppins-ExtraBold.ttf"),
+    PoppinsRegular: require("../../../../assets/fonts/Poppins-Regular.ttf"),
+  });
 
-    if (!loaded) {
-        return null;
-    }
-    return (
-        <TouchableOpacity
-            key={userID}
-            style={styles.card}
-            onPress={() => {
-                navigation.getParent().navigate('Profile', {
-                    user: userID
-                })
-            }}
-        >
-            <ImageBackground
-                resizeMode={"cover"}
-                source={typeof (picture) === "string" && picture !== "" ? {uri: picture} : require("../../../../assets/images/avatar-300x300.jpg")}
-                style={styles.userImg}
-            >
-                <View style={styles.userContent}>
-                    <Image style={styles.userContentBackground}/>
-                    <View style={styles.userContentRow}>
-                        <Text style={styles.detailBtn}>
-                            <Text style={styles.detailBtnText}>{name}</Text>
-                        </Text>
-                    </View>
-                </View>
-            </ImageBackground>
-        </TouchableOpacity>
-    );
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <TouchableOpacity
+      key={userID}
+      style={styles.card}
+      onPress={() => {
+        navigation.getParent().navigate("Profile", {
+          user: userID,
+        });
+      }}
+    >
+      <ImageBackground
+        resizeMode={"cover"}
+        source={{ uri: picture }}
+        style={styles.userImg}
+      >
+        <View style={styles.userContent}>
+          <Image style={styles.userContentBackground} />
+          <View style={styles.userContentRow}>
+            <Text style={styles.detailBtn}>
+              <Text style={styles.detailBtnText}>{name}</Text>
+            </Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
 };
 
 export default CommunityCardSmall;
--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,154 +1,154 @@
 import {
-    Image,
-    View,
-    Text,
-    TouchableOpacity,
-    ScrollView,
-    Linking,
-    Pressable,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Linking,
+  Pressable,
 } from "react-native";
 import styles from "./styles";
-import {AntDesign} from "@expo/vector-icons";
-import {WHITE} from "../../styles/colors";
+import { AntDesign } from "@expo/vector-icons";
+import { WHITE } from "../../styles/colors";
 
-import {auth, db} from "../../config/firebase";
-import {useEffect, useState} from "react";
-import {doc, getDoc} from "firebase/firestore";
-import {useFonts} from "expo-font";
-import {signOut} from "../../api/loginApi";
+import { auth, db } from "../../config/firebase";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { useFonts } from "expo-font";
+import { signOut } from "../../api/loginApi";
 
 // const data = require("../../../assets/mockJSON/MOCK_DATA.json");
 
-const Profile = ({navigation}) => {
-    const [data, setData] = useState({});
-    const getData = () => {
-        getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
-            if (docSnap.exists()) {
-                setData(docSnap.data());
-            } else {
-                console.log("No such document!");
-            }
-        });
-    };
-    useEffect(() => {
-        getData();
-    }, []);
-    const [loaded, error] = useFonts({
-        PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
-        PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
+const Profile = ({ navigation }) => {
+  const [data, setData] = useState({});
+  const getData = () => {
+    getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+      if (docSnap.exists()) {
+        setData(docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
     });
-    if (!loaded) {
-        return null;
-    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const [loaded, error] = useFonts({
+    PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
 
-    return (
-        <ScrollView showsVerticalScrollIndicator={false} style={styles.background}>
-            <View style={styles.topBar}>
-                <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate("Community");
-                    }}
-                >
-                    <AntDesign
-                        name={"left"}
-                        size={24}
-                        color={WHITE}
-                        style={styles.backBtn}
-                    />
-                </TouchableOpacity>
-                <Text style={styles.title}>Profile</Text>
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.background}>
+      <View style={styles.topBar}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Community");
+          }}
+        >
+          <AntDesign
+            name={"left"}
+            size={24}
+            color={WHITE}
+            style={styles.backBtn}
+          />
+        </TouchableOpacity>
+        <Text style={styles.title}>Profile</Text>
+      </View>
+      <View>
+        <View style={styles.profileImageWrapper}>
+          <Image
+            source={{
+              uri: data.avatar,
+            }}
+            style={styles.profileImage}
+          />
+        </View>
+        <View style={styles.userContentDisplay}>
+          <View style={styles.userContentRow}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>Display Name</Text>
             </View>
-            <View>
-                <View style={styles.profileImageWrapper}>
-                    <Image
-                        source={{
-                            uri: typeof data.avatar === "string" && data.avatar !== "" ? data.avatar : require("../../../assets/images/avatar-300x300.jpg"),
-                        }}
-                        style={styles.profileImage}
-                    />
-                </View>
-                <View style={styles.userContentDisplay}>
-                    <View style={styles.userContentRow}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>Display Name</Text>
-                        </View>
-                        <View style={styles.userContentWrapper}>
-                            <Text style={styles.userContent}>{data.fullName}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.userContentRow}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>Date of birth</Text>
-                        </View>
-                        <View style={styles.userContentWrapper}>
-                            <Text style={styles.userContent}>{data.dob}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.userContentRow}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>E-mail</Text>
-                        </View>
-                        <View style={styles.userContentWrapper}>
-                            <Text style={styles.userContent}>{data.email}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.userContentRow}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>Campus</Text>
-                        </View>
-                        <View style={styles.userContentWrapper}>
-                            <Text style={styles.userContent}>{data.campus}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.userContentRow}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>Interest in</Text>
-                        </View>
-                        <View style={styles.userContentWrapper}>
-                            <Text style={styles.userContent}>
-                                {data.subjects && data.subjects.join(" | ")}
-                            </Text>
-                        </View>
-                    </View>
-                    <View style={styles.userContentRow}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>Facebook Link</Text>
-                        </View>
-                        <Pressable style={styles.userContentWrapper}>
-                            <Text
-                                style={styles.hyperlink}
-                                onPress={() => Linking.openURL("https://" + data.facebook)}
-                            >
-                                {data.facebook}
-                            </Text>
-                        </Pressable>
-                    </View>
-                    <View style={[styles.userContentRow, styles.userContentLastRow]}>
-                        <View style={styles.userContentHeadingWrapper}>
-                            <Text style={styles.userContentHeading}>Bio</Text>
-                        </View>
-                        <View style={styles.userContentWrapper}>
-                            <Text style={styles.userContent}>{data.bio}</Text>
-                        </View>
-                    </View>
-                </View>
+            <View style={styles.userContentWrapper}>
+              <Text style={styles.userContent}>{data.fullName}</Text>
             </View>
-            {auth.currentUser.uid ? (
-                <View style={styles.logoutBtnWrapper}>
-                    <TouchableOpacity
-                        style={styles.logoutBtn}
-                        onPress={() => {
-                            signOut({navigation});
-                        }}
-                    >
-                        <Text style={styles.logoutBtnText}>Logout</Text>
-                    </TouchableOpacity>
-                </View>
-            ) : (
-                console.log("id: " + auth.currentUser.uid)
-            )}
-        </ScrollView>
-    );
+          </View>
+          <View style={styles.userContentRow}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>Date of birth</Text>
+            </View>
+            <View style={styles.userContentWrapper}>
+              <Text style={styles.userContent}>{data.dob}</Text>
+            </View>
+          </View>
+          <View style={styles.userContentRow}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>E-mail</Text>
+            </View>
+            <View style={styles.userContentWrapper}>
+              <Text style={styles.userContent}>{data.email}</Text>
+            </View>
+          </View>
+          <View style={styles.userContentRow}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>Campus</Text>
+            </View>
+            <View style={styles.userContentWrapper}>
+              <Text style={styles.userContent}>{data.campus}</Text>
+            </View>
+          </View>
+          <View style={styles.userContentRow}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>Interest in</Text>
+            </View>
+            <View style={styles.userContentWrapper}>
+              <Text style={styles.userContent}>
+                {data.subjects && data.subjects.join(" | ")}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.userContentRow}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>Facebook Link</Text>
+            </View>
+            <Pressable style={styles.userContentWrapper}>
+              <Text
+                style={styles.hyperlink}
+                onPress={() => Linking.openURL("https://" + data.facebook)}
+              >
+                {data.facebook}
+              </Text>
+            </Pressable>
+          </View>
+          <View style={[styles.userContentRow, styles.userContentLastRow]}>
+            <View style={styles.userContentHeadingWrapper}>
+              <Text style={styles.userContentHeading}>Bio</Text>
+            </View>
+            <View style={styles.userContentWrapper}>
+              <Text style={styles.userContent}>{data.bio}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      {auth.currentUser.uid ? (
+        <View style={styles.logoutBtnWrapper}>
+          <TouchableOpacity
+            style={styles.logoutBtn}
+            onPress={() => {
+              signOut({ navigation });
+            }}
+          >
+            <Text style={styles.logoutBtnText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        console.log("id: " + auth.currentUser.uid)
+      )}
+    </ScrollView>
+  );
 };
 
 export default Profile;
