a5fd57a3b43a93f1e5837b0a5a59ec34d33ea6f3
Thu Tran <ngocanhthu20102002@gmail.com>
2022-04-28 13:37:25 +0700

    + added 3 onboarding screens + added skip button + added next button + added get started button + navigate to sign in screen

--- src/screens/edit-profile/styles.js
+++ src/screens/edit-profile/styles.js
@@ -1,9 +1,7 @@
 import { StyleSheet } from "react-native";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 import {
-  ALERT_PURPLE_BUTTON,
   BACKGROUND,
-  BLACK,
   DARK_BLUE_PURPLE,
   GREY,
   LIGHT_BLUE,
