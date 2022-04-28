b39ccb90fe804b5a694858378cffc480adbbbb17
Thu Tran <ngocanhthu20102002@gmail.com>
2022-04-28 15:48:49 +0700

    added async storage for persistence

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import AuthProvider from "./AuthProvider";
 
 export default function App() {
   Text.defaultProps = Text.defaultProps || {};
   Text.defaultProps.allowFontScaling = false;
 
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,97 +1,100 @@
-import {Text, View, ImageBackground} from "react-native";
+import { Text, View, ImageBackground } from "react-native";
 import styles from "./styles";
-import {TouchableOpacity} from "react-native";
-import {useFonts} from "expo-font";
+import { TouchableOpacity } from "react-native";
+import { useFonts } from "expo-font";
+import { useContext } from "react";
+import { AuthContext } from "../../../AuthProvider";
 
-const HomeScreen = ({navigation}) => {
-    // handle font
-    const [loaded, error] = useFonts({
-        PoppinsSemiBold: require("../../../assets/fonts/Poppins-SemiBold.ttf"),
-        PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
-        PoppinsMedium: require("../../../assets/fonts/Poppins-Medium.ttf"),
-        PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
-        PoppinsExtraBold: require("../../../assets/fonts/Poppins-ExtraBold.ttf"),
-    });
-    if (!loaded) {
-        return null;
-    }
+const HomeScreen = ({ navigation }) => {
+  const { setIsLoggedIn } = useContext(AuthContext);
+  // handle font
+  const [loaded, error] = useFonts({
+    PoppinsSemiBold: require("../../../assets/fonts/Poppins-SemiBold.ttf"),
+    PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsMedium: require("../../../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsBold: require("../../../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsExtraBold: require("../../../assets/fonts/Poppins-ExtraBold.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.melbourneBackpackText}>Melbourne Backpack</Text>
-            <View style={styles.furtherButton}>
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() =>
-                        navigation.navigate("Content", {screen: "Community"})
-                    }
-                >
-                    <ImageBackground
-                        resizeMode={"cover"}
-                        source={require("../../../assets/images/community.jpg")}
-                        style={styles.backgroundImage}
-                        imageStyle={{borderRadius: 20}}
-                    >
-                        <View style={styles.overlay}>
-                            <Text style={styles.subtitle}>Community</Text>
-                        </View>
-                    </ImageBackground>
-                </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.melbourneBackpackText}>Melbourne Backpack</Text>
+      <View style={styles.furtherButton}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() =>
+            navigation.navigate("Content", { screen: "Community" })
+          }
+        >
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/community.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Community</Text>
             </View>
-            <View style={styles.furtherButton}>
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() => navigation.navigate("Content", {screen: "Housing"})}
-                >
-                    <ImageBackground
-                        resizeMode={"cover"}
-                        source={require("../../../assets/images/housing.jpg")}
-                        style={styles.backgroundImage}
-                        imageStyle={{borderRadius: 20}}
-                    >
-                        <View style={styles.overlay}>
-                            <Text style={styles.subtitle}>Housing</Text>
-                        </View>
-                    </ImageBackground>
-                </TouchableOpacity>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.furtherButton}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => navigation.navigate("Content", { screen: "Housing" })}
+        >
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/housing.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Housing</Text>
             </View>
-            <View style={styles.furtherButton}>
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() => navigation.navigate("Content", {screen: "Shopping"})}
-                >
-                    <ImageBackground
-                        resizeMode={"cover"}
-                        source={require("../../../assets/images/shopping.jpg")}
-                        style={styles.backgroundImage}
-                        imageStyle={{borderRadius: 20}}
-                    >
-                        <View style={styles.overlay}>
-                            <Text style={styles.subtitle}>Shopping</Text>
-                        </View>
-                    </ImageBackground>
-                </TouchableOpacity>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.furtherButton}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => navigation.navigate("Content", { screen: "Shopping" })}
+        >
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/shopping.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Shopping</Text>
             </View>
-            <View style={styles.furtherButton}>
-                <TouchableOpacity
-                    activeOpacity={0.5}
-                    onPress={() =>
-                        navigation.navigate("Content", {screen: "Transport"})
-                    }
-                >
-                    <ImageBackground
-                        resizeMode={"cover"}
-                        source={require("../../../assets/images/transport.jpg")}
-                        style={styles.backgroundImage}
-                        imageStyle={{borderRadius: 20}}
-                    >
-                        <View style={styles.overlay}>
-                            <Text style={styles.subtitle}>Transport</Text>
-                        </View>
-                    </ImageBackground>
-                </TouchableOpacity>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.furtherButton}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() =>
+            navigation.navigate("Content", { screen: "Transport" })
+          }
+        >
+          <ImageBackground
+            resizeMode={"cover"}
+            source={require("../../../assets/images/transport.jpg")}
+            style={styles.backgroundImage}
+            imageStyle={{ borderRadius: 20 }}
+          >
+            <View style={styles.overlay}>
+              <Text style={styles.subtitle}>Transport</Text>
             </View>
-          
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+
       <View style={styles.furtherButton}>
         <TouchableOpacity
           activeOpacity={0.5}
@@ -106,11 +109,11 @@ const HomeScreen = ({navigation}) => {
             <View style={styles.overlay}>
               <Text style={styles.subtitle}>Profile</Text>
             </View>
-</ImageBackground>
+          </ImageBackground>
         </TouchableOpacity>
       </View>
-        </View>
-    );
+    </View>
+  );
 };
 
 export default HomeScreen;
