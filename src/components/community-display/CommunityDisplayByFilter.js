cd252c95e5575a101fe8dbfbbc4a549285483138
rider3458 <s3878480@rmit.edu.vn>
2022-04-30 16:05:07 +0700

    upload CommunityDisplayByFilter.js

---
+++ src/components/community-display/CommunityDisplayByFilter.js
@@ -0,0 +1,103 @@
+import {FlatList, Text, View, TouchableOpacity, SafeAreaView, TouchableOpacityComponent} from "react-native";
+import styles from "./styles";
+import {useEffect, useRef, useState} from "react";
+import CommunityCardLarge from "../community-card/community-card-large/CommunityCardLarge";
+import CommunityScreenSeeMore from "./community-see-more/CommunityScreenSeeMore";
+import Modal from "react-native-modal";
+import {useIsFocused} from "@react-navigation/native";
+
+const CommunityDisplayByFilter = ({heading, optionList, userList, filterMaxCards, navigation}) => {
+    const [selectedOption, setSelectedOption] = useState(optionList[0].name)
+    const [temp, setTemp] = useState(userList)
+    const [showMore, setShowMore] = useState(false)
+    const [firstTime, setFirstTime] = useState(true)
+    const options = optionList
+    const isFocused = useIsFocused();
+    useEffect(() => {
+        const filterCards = []
+        userList.map((item) => {
+            let check = 0
+            if (item[heading] === selectedOption || item[heading].includes(selectedOption)) check++
+            if (check > 0) filterCards.push(item)
+        })
+        setTemp(filterCards)
+    }, [selectedOption])
+
+    useEffect(() => {
+        setShowMore(false)
+    }, [isFocused])
+
+    return (
+        <View key={heading}>
+            <Modal isVisible={showMore} transparent={false} propagateSwipe={true}
+                   style={styles.modal}>
+                <CommunityScreenSeeMore option={selectedOption} data={temp} heading={heading} show={setShowMore}
+                                        navigation={navigation} style={styles.communitySeeMorePopup}/>
+            </Modal>
+            <View style={styles.headingRow}>
+                <View>
+                    <Text style={styles.filterText}>
+                        {"Filtered by " + heading.slice(0, 1).toUpperCase() +
+                            heading.slice(1, heading.length)}
+                    </Text>
+                </View>
+                <View style={styles.seeMoreWrapper}>
+                    <TouchableOpacity onPress={() => {
+                        setShowMore(true)
+                        setFirstTime(false)
+                    }}>
+                        <Text style={styles.seeMoreText}>
+                            See more
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.optionWrapper}>
+                <FlatList data={options} extraData={options} keyExtractor={option => option.id} horizontal={true}
+                          renderItem={(option) => {
+                              if (option.item.name !== selectedOption) return (
+                                  <TouchableOpacity onPress={() => {
+                                      setSelectedOption(option.item.name)
+                                  }}>
+                                      <Text style={styles.optionText}>
+                                          {option.item.name}
+                                      </Text>
+                                  </TouchableOpacity>
+                              )
+                              if (option.item.name === selectedOption) return (
+                                  <TouchableOpacity>
+                                      <Text style={styles.optionTextSelected}>
+                                          {option.item.name}
+                                      </Text>
+                                  </TouchableOpacity>
+                              )
+                          }
+                          }/>
+            </View>
+            <SafeAreaView style={styles.communityList}>
+                {
+                    <FlatList
+                        extraData={temp}
+                        data={temp.slice(0, filterMaxCards)}
+                        horizontal={true}
+                        numColumns={1}
+                        renderItem={(user) => {
+                            return (
+                                <CommunityCardLarge
+                                    userID={user.item.id}
+                                    name={user.item.fullName}
+                                    campus={user.item.campus}
+                                    picture={user.item.avatar}
+                                    topic={user.item.subjects ? user.item.subjects[Math.floor(Math.random() * 2)] : null}
+                                    navigation={navigation}
+                                />
+                            );
+                        }}
+                    />
+                }
+            </SafeAreaView>
+        </View>
+    )
+}
+
+export default CommunityDisplayByFilter
