24b6a1bc5251f48ea35016b0e7ef870fcd8c607b
Thu Tran <ngocanhthu20102002@gmail.com>
2022-05-03 14:50:02 +0700

    deleted unnescessary files

--- App.js
+++ App.js
@@ -2,17 +2,14 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
-import AuthProvider from "./AuthProvider";
 
 export default function App() {
   Text.defaultProps = Text.defaultProps || {};
   Text.defaultProps.allowFontScaling = false;
 
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
