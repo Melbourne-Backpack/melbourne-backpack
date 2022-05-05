f37d2bd2e1dd3efd0e6311f78d7a59a7bd839801
doanyennhi <s3880599@rmit.edu.vn>
2022-05-06 03:34:15 +0700

    More styling for alert

--- src/components/housing-details/alert/SubmitAlert.js
+++ src/components/housing-details/alert/SubmitAlert.js
@@ -1,11 +1,14 @@
 import Modal from "react-native-modal";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import styles from "./styles";
+import React, { useState } from "react";
 
 const SubmitAlert = () => {
+  const [isVisible, setVisible] = useState(true);
+
   return (
     <Modal
-      isVisible={true}
+      isVisible={isVisible}
       animationIn={"slideInUp"}
       animationOut={"slideOutDown"}
     >
@@ -16,7 +19,25 @@ const SubmitAlert = () => {
               width: "100%",
             }}
           >
+            <View style={styles.headerContainer}>
+              <Image
+                source={require("../../../../assets/success-icon.png")}
+                style={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+              <Text style={styles.headerText}>SUCCESS</Text>
+            </View>
+
             <Text style={styles.text}>Your review has been submitted!</Text>
+
+            <TouchableOpacity
+              onPress={() => setVisible(!isVisible)}
+              style={styles.cancelButton}
+            >
+              <Text style={styles.cancelButtonText}>OK</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
--- src/components/housing-details/alert/styles.js
+++ src/components/housing-details/alert/styles.js
@@ -14,12 +14,36 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width: wp("80%"),
   },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 20,
+  },
+  headerText: {
+    color: "#25AE88",
+    fontFamily: "PoppinsBlack",
+    fontSize: 18,
+    marginHorizontal: 5,
+  },
   text: {
     textAlign: "center",
     color: WHITE,
-    fontFamily: "PoppinsMedium",
+    fontFamily: "PoppinsRegular",
     fontSize: 15,
     paddingVertical: 15,
+  },
+  cancelButton: {
+    width: wp("80%"),
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  cancelButtonText: {
+    color: WHITE,
+    fontFamily: "PoppinsBlack",
+    fontSize: 17,
+    textAlign: "center",
+    paddingVertical: 10,
   },
 });
 
