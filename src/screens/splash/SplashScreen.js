0ffe0d0dc949685f7814b370d45627c49965045f
Hoang Linh <hlinh.jobs@gmail.com>
2022-05-11 17:15:29 +0700

    upgrade SplashScreen.js

--- src/screens/splash/SplashScreen.js
+++ src/screens/splash/SplashScreen.js
@@ -1,37 +1,54 @@
 import { Image, ImageBackground, Text, View } from "react-native";
 import styles from "./styles";
 import { useFonts } from "expo-font";
+import { widthPercentageToDP } from "react-native-responsive-screen";
 
 // use replace instead of navigate to avoid the back button on Android
 const SplashScreen = ({ navigation }) => {
   const [loaded, error] = useFonts({
     PoppinsExtraBold: require("../../../assets/fonts/Poppins-ExtraBold.ttf"),
     PoppinsRegular: require("../../../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsMedium: require("../../../assets/fonts/Poppins-Medium.ttf"),
   });
 
   if (!loaded) {
     return null;
   }
 
-  setTimeout(() => {
-    navigation.replace("Onboarding");
-  }, 3000);
+  // setTimeout(() => {
+  //   navigation.replace("Onboarding");
+  // }, 3000);
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../../../assets/images/melbourne-bg.jpg")}
-        style={styles.backgroundImage}
-      >
+    <ImageBackground
+      source={require("../../../assets/images/melbourne-bg.jpg")}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.content}>
         <Image
-          source={require("../../../assets/adaptive-icon.png")}
+          source={{
+            uri: "https://media4.giphy.com/media/h8gfzy5zmIEULnetC1/giphy.gif?cid=ecf05e47p7vm16248f5jql6e1q638ag2x6l1ufv7fd8lu14d&rid=giphy.gif&ct=s",
+          }}
           style={styles.icon}
         />
-        <Text style={styles.textTitle}>
-          Melbourne {"\n"} Backpack {"\n"}
-        </Text>
+        {/*<Image*/}
+        {/*  source={require("../../../assets/adaptive-icon.png")}*/}
+        {/*  style={styles.icon}*/}
+        {/*/>*/}
+        <Text style={styles.textTitle}>Melbourne {"\n"} Backpack</Text>
+        <Text style={styles.description}>A mini-Melbourne in your pocket!</Text>
+      </View>
+      <View style={styles.holdText}>
+        <Image
+          source={require("../../../assets/Splash-1.png")}
+          style={{
+            width: widthPercentageToDP(85),
+            height: 200,
+            marginBottom: 30,
+          }}
+        />
         <Text style={styles.versionText}>Version 1.0</Text>
-      </ImageBackground>
-    </View>
+      </View>
+    </ImageBackground>
   );
 };
 
